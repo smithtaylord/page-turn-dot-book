bdc686f7c3f388bcb44b1df48be8984ff8a77285
Payton Boone <paytonboone30@gmail.com>
2023-03-08 09:46:19 -0700

    Club model setup

--- page-turn-dot-book/server/db/DbContext.js
+++ page-turn-dot-book/server/db/DbContext.js
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
+import { ClubSchema } from '../models/Club.js';
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+
+  Clubs = mongoose.model('Clubs', ClubSchema)
 }
 
 export const dbContext = new DbContext()
--- page-turn-dot-book/server/models/Club.js
+++ page-turn-dot-book/server/models/Club.js
@@ -1,7 +1,20 @@
 import { Schema } from "mongoose";
+import { basicStringType, defaultSchemaOptions } from "../db/Constants.js";
 
 export const ClubSchema = new Schema(
     {
-
-    }
+        name: basicStringType,
+        bio: basicStringType,
+        coverImg: basicStringType,
+        // type: {...basicStringType, enum: ['speed-readers', 'casual-readers', 'cat-moms', 'history-buffs', 'fiction-fans', 'other']}
+        
+        creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true},
+    }, defaultSchemaOptions
 )
+
+ClubSchema.virtual('creator', {
+    ref: 'Account',
+    localField: 'creatorId',
+    foreignField: '_id',
+    justOne: true,
+})
