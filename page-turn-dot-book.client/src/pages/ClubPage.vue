f66e091dcd75ab4cb2e417c308393947cbd90491
Jaden Hawkes <jadenhawkes@gmail.com>
2023-03-09 14:50:03 -0700

    Styling

--- page-turn-dot-book.client/src/pages/ClubPage.vue
+++ page-turn-dot-book.client/src/pages/ClubPage.vue
@@ -3,15 +3,17 @@
         <div class="row">
             <div class="col-12">
                 <div class="card text-start">
-                    <img class="clubImg" :src="club.coverImg" :alt="club.name">
+                    <img class="clubImg" :src="club?.coverImg" :alt="club?.name">
                     <div class="card-body">
                         <h4 class="card-title">{{ club.name }}</h4>
                         <p class="card-text">{{ club.bio }}</p>
                     </div>
                     <div class="row justify-content-end">
                         <div class="col-6 text-end m-2">
-                            <button class="btn bg-success" v-if="!foundMember" @click="createMember()" :disabled="club.isArchived">Join Club</button>
-                            <button class="btn bg-danger" v-else @click="removeMember(foundMember.memberId)" :disabled="club.isArchived" >Leave Club</button>
+                            <button class="btn bg-success" v-if="!foundMember" @click="createMember()"
+                                :disabled="club.isArchived">Join Club</button>
+                            <button class="btn bg-danger" v-else @click="removeMember(foundMember.memberId)"
+                                :disabled="club.isArchived">Leave Club</button>
                         </div>
                     </div>
                 </div>
@@ -20,10 +22,23 @@
                 <h4>Club Members</h4>
                 <div class="d-flex">
                     <div v-for="m in members">
-                        <img :src="m.picture" :alt="m-name" class="profilePic m-1">
+                        <img :src="m.picture" :alt="m - name" class="profilePic m-1">
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
+    <div class="container-fluid my-3">
+        <div class="row">
+            <div class="col-12 text-center bg-primary">
+                <h1>Currently Reading</h1>
+                <p> PLACEHOLDER </p>
+                <h4 class="m-0"> Souper Cool Book</h4>
+                <h6 class="mb-3"> Author McAuthor</h6>
+            </div>
+        </div>
+        <div class="row">
+            <CommentComponent />
         </div>
     </div>
 </template>
@@ -38,6 +53,7 @@ import { AppState } from '../AppState.js'
 import ClubCard from '../components/ClubCard.vue';
 import { useRoute, useRouter } from 'vue-router';
 import { clubMembersService } from '../services/ClubMembersService';
+import CommentComponent from "../components/CommentComponent.vue";
 
 export default {
     setup() {
@@ -54,7 +70,7 @@ export default {
                 router.push("/");
             }
         }
-        async function getMembersByClubId(){
+        async function getMembersByClubId() {
             try {
                 const clubId = route.params.clubId
                 await clubMembersService.getMembersByClubId(clubId)
@@ -64,7 +80,7 @@ export default {
         }
 
         watchEffect(() => {
-            if (route.params.clubId){
+            if (route.params.clubId) {
                 getClubById();
                 getMembersByClubId();
             }
@@ -77,17 +93,17 @@ export default {
             foundMember: computed(() => AppState.members.find(m => m.id == AppState.account.id)),
             myMembership: computed(() => AppState.members.find(m => m.clubId == AppState.activeClub.id)),
             account: computed(() => AppState.account),
-            async createMember(){
+            async createMember() {
                 try {
-                    await clubMembersService.createMember({clubId: route.params.clubId})
+                    await clubMembersService.createMember({ clubId: route.params.clubId })
                 } catch (error) {
                     Pop.error('creating member')
                     logger.error(error)
                 }
             },
-            async removeMember(memberId){
+            async removeMember(memberId) {
                 try {
-                    if(await Pop.confirm()){
+                    if (await Pop.confirm()) {
                         await clubMembersService.removeMember(memberId)
                         logger.log(memberId)
                     }
@@ -98,7 +114,7 @@ export default {
             }
         };
     },
-    components: { ClubCard }
+    components: { ClubCard, CommentComponent }
 }
 </script>
 
@@ -110,10 +126,30 @@ export default {
 
 }
 
-.profilePic{
+.profilePic {
     height: 50px;
     width: 50px;
     border-radius: 50%;
     box-shadow: 1px 1px 2px black;
+}
+
+.input {
+    max-width: 200px;
+    min-height: 40px;
+    padding: 10px;
+    font-family: 'Courier New', Courier, monospace;
+    outline: none;
+    background: #e8e8e8;
+    box-shadow: 5px 5px 17px #c8c8c8,
+        -5px -5px 17px #ffffff;
+    border: none;
+    border-radius: 10px;
+    transition: all .5s;
+}
+
+.input:focus {
+    background: #e8e8e8;
+    box-shadow: inset 5px 5px 17px #c8c8c8,
+        inset -5px -5px 17px #ffffff;
 }
 </style>
