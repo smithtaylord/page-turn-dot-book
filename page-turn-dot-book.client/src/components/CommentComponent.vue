fff304832b75ecf121761839a00927476c32e91f
Taylor Smith <smith.taylord@gmail.com>
2023-03-13 12:27:39 -0600

    lunch time

--- page-turn-dot-book.client/src/components/CommentComponent.vue
+++ page-turn-dot-book.client/src/components/CommentComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="component">
         <div class="">
-            <form @submit.prevent="handleSubmit()" class="">
+            <!-- <form @submit.prevent="handleSubmit()" class="">
                 <div class="align-items-center">
                     <input v-model="editable.body" placeholder="Leave a comment..." class="input" name="comment"
                         type="text">
@@ -9,16 +9,17 @@
                         <i class="mdi mdi-send"> </i>
                     </button>
                 </div>
-            </form>
-            <!-- <form @submit.prevent="handleSubmit()">
+            </form> -->
+            <form @submit.prevent="handleSubmit()">
                 <div class="py-3">
-                    <textarea required v-model="editable.body" class="form-control p-2" placeholder="Tell the people..."
-                        style="height: 100px" maxlength="2500" minlength="2"></textarea>
+                    <textarea required v-model="editable.body" class="form-control p-2"
+                        placeholder="Join the conversation! Leave your comments on the book below...." style="height: 100px"
+                        maxlength="2500" minlength="2"></textarea>
                     <div class="d-flex justify-content-end py-3">
-                        <button class="btn bg-success text-dark fw-bold" type="submit">post comment</button>
+                        <button class="btn bg-success text-dark fw-bold box-shadow" type="submit">post comment</button>
                     </div>
                 </div>
-            </form> -->
+            </form>
         </div>
     </div>
     <div class="container-fluid bg-dark">
@@ -26,12 +27,12 @@
             <div v-for="c in comments" class="col-12 my-3">
                 <div class="row d-flex align-content-center">
                     <div class="col-2">
-                        <img :src="c?.creator?.picture" alt="" class="profilePic">
+                        <img :src="c?.creator.picture" alt="" class="profilePic">
                     </div>
                     <div class="col-8 bg-warning ms-2 rounded">
-                        <p class="fw-bold m-0"> {{ c?.creator?.name }}</p>
+                        <p class="fw-bold m-0"> {{ c?.creator.name }}</p>
                         <p class="m-0 mb-2">
-                            {{ c.body }}
+                            {{ c?.body }}
                         </p>
                     </div>
                 </div>
@@ -54,10 +55,8 @@ export default {
         const editable = ref({})
         const route = useRoute()
         watchEffect(() => {
-
             if (route.params.clubId) {
                 getCommentsByClubId();
-                
             }
         })
         async function getCommentsByClubId() {
--- page-turn-dot-book.client/src/services/CommentsService.js
+++ page-turn-dot-book.client/src/services/CommentsService.js
@@ -9,17 +9,17 @@ import { api } from "./AxiosService.js"
 class CommentsService {
     async getCommentsByClubId(clubId) {
         const res = await api.get('api/clubs/' + clubId + '/comments')
-        logger.log(res.data)
+        // logger.log(res.data)
         AppState.comments = res.data.map(c => new Comment(c))
 
     }
 
     async createComment(commentData) {
-        logger.log(commentData)
+        // logger.log(commentData)
         const res = await api.post('api/comments', commentData)
         AppState.comments.push(new Comment(res.data))
-        
-        return res.data
+
+        // return res.data
     }
 
 }
