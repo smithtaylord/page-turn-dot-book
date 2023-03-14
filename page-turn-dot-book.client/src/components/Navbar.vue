69ff44a9564041147e3ead4ed12fd1331c5a95ec
Thomas Peek <peekthomas369@icloud.com>
2023-03-14 16:06:08 -0600

    uber cool navbar for desktop

--- page-turn-dot-book.client/src/components/Navbar.vue
+++ page-turn-dot-book.client/src/components/Navbar.vue
@@ -1,16 +1,20 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-      <h1>
-        <i class="mdi mdi-book-open-variant fs-1 text-light"></i>
-        PageTurn.Book
-      </h1>
-    </router-link>
-
-    <button class="btn bg-dark selectable" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"
-      aria-controls="offcanvasExample">
-      <i class="fs-1 mdi mdi-menu"></i>
-    </button>
+  <nav class="navbar navbar-expand-xxl navbar-dark bg-dark px-3">
+    <div>
+      <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
+        <h1 class="">
+          <i class="mdi mdi-book-open-variant fs-1 text-light"></i>
+          PageTurn.Book
+        </h1>
+      </router-link>
+    </div>
+    <div class="justify-content-end displayButton" >
+      <button class="btn bg-dark selectable" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"
+        aria-controls="offcanvasExample">
+        <i class="fs-1 mdi mdi-menu"></i>
+      </button>
+    </div>
+  </nav>
     <!-- OLD NAVBAR STUFF BELOW LEFT FOR REF -->
 
     <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
@@ -27,7 +31,6 @@
       </ul>
       <Login />
     </div> -->
-  </nav>
 </template>
 
 <script>
@@ -41,6 +44,13 @@ export default {
 </script>
 
 <style scoped>
+
+
+.displayButton{
+  justify-self: inherit;
+  overflow: hidden;
+}
+
 a:hover {
   text-decoration: none;
 }
@@ -55,9 +65,9 @@ a:hover {
   border-bottom-right-radius: 0;
 }
 
-@media screen and (min-width: 768px) {
+/* @media screen and (min-width: 768px) {
   nav {
     height: 64px;
   }
-}
+} */
 </style>
