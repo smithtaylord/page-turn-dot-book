ebc313ca9798c88a57927ea4c4eb78a6f6a728d2
Thomas Peek <peekthomas369@icloud.com>
2023-03-14 16:59:34 -0600

    j

--- page-turn-dot-book.client/src/pages/HomePage.vue
+++ page-turn-dot-book.client/src/pages/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-12 homePagePic ">
+      <div class="col-12 d-sm-block d-md-none homePagePic ">
         <p class="text-light py-5 px-2 fs-3 text-shadow">
           At PageTurn.Book, we offer tools for hosting
           book clubs and tools for finding book club
@@ -15,6 +15,20 @@
           </router-link>
         </div>
       </div>
+            <div class="col-12 d-none d-md-block d-xxl-block LGhomePagePic">
+          <p class="text-light py-5 px-2 fs-3 text-shadow">
+            At PageTurn.Book, we offer tools for hosting
+            book clubs and tools for finding book club
+            members.
+          </p>
+          <div v-if="account.id">
+            <router-link class="selectable" :to="{ name: 'CreateClub' }">
+              <button class="mb-3 btn bg-danger selectable">
+                Create a Club
+              </button>
+            </router-link>
+          </div>
+        </div>
     </div>
   </div>
   <div class="container-fluid">
@@ -95,6 +109,11 @@ export default {
   background-image: url(https://images.unsplash.com/photo-1460518451285-97b6aa326961?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80);
   background-size: cover;
 
+}
+
+.LGhomePagePic {
+  background-image: url(public/bookbackground.jpeg);
+  background-size: cover;
 }
 
 .scroll-x {
