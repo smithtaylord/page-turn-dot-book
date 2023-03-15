3a40a092da5ca841df18e44c22f12295535fa742
Thomas Peek <peekthomas369@icloud.com>
2023-03-15 10:10:50 -0600

    clubbook model change

--- page-turn-dot-book/server/models/ClubBook.js
+++ page-turn-dot-book/server/models/ClubBook.js
@@ -1,5 +1,6 @@
-import { Schema } from "mongoose";
+import { Schema, SchemaType, SchemaTypeOptions } from "mongoose";
 import { basicStringType, defaultSchemaOptions } from "../db/Constants";
+
 
 export const ClubBookSchema = new Schema(
     {
@@ -8,6 +9,7 @@ export const ClubBookSchema = new Schema(
         coverImg: basicStringType,
         isbn: basicStringType,
         isArchived: { type: Boolean, required: true, default: false },
+        voteId: { type: [Schema.Types.ObjectId], default: []},
         // genre: basicStringType,
         // isActive: {type: Boolean, required: true, default: false}
         // NOTE come back and maybe add votes somehow, maybe
